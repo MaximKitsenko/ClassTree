02e9a5ef9979ac354aae79dfd9d774f307ffb84f
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-09-06 15:58:49 +0300

    output fixed

--- ClassTree/Program.fs
+++ ClassTree/Program.fs
@@ -3,17 +3,18 @@
 
 let getAssembly assemblyPath = System.Reflection.Assembly.LoadFile(assemblyPath)
 let getAssemblyClasses (assembly: System.Reflection.Assembly) = assembly.GetTypes()
-let printfMemberOfSystemType (m:System.Reflection.MemberInfo) = printfn "%s:%s" m.Name (m.MemberType.ToString())
+let printfMemberOfSystemType (m:System.Reflection.FieldInfo) = printfn "%s:%s" m.Name (m.FieldType.ToString())
 
 let rec printfCustomType (m:System.Reflection.MemberInfo) = 
     printfn "%s:[CustomType]" m.Name 
     printClass (m.MemberType.GetType())
 
 and printClass (c: System.Type )= 
-    printfn "%s" c.Name
-    let systemTypes, customTypes =  c.GetMembers() |> Array.toList |> List.partition ( fun cm -> cm.ReflectedType.Namespace = "System")
-    systemTypes |> List.sortBy (fun (x:System.Reflection.MemberInfo) -> x.Name) |> List.iter printfMemberOfSystemType
-    customTypes |> List.sortBy (fun (x:System.Reflection.MemberInfo) -> x.Name) |> List.iter printfCustomType
+    printfn "%s" c.Name  
+    let temp = c.GetFields( System.Reflection.BindingFlags.NonPublic ||| System.Reflection.BindingFlags.Instance )|> Array.filter (fun x -> (x.MemberType.ToString()) = "Field" ) |> Array.toList
+    let systemTypes, customTypes =  temp|> List.partition ( fun cm -> cm.FieldType.Namespace = "System")
+    systemTypes |> List.sortBy (fun x -> x.Name) |> List.iter printfMemberOfSystemType
+    customTypes |> List.sortBy (fun x -> x.Name) |> List.iter printfCustomType
 
 [<EntryPoint>]
 let main argv = 
