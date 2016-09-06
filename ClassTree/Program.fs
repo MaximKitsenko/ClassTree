da4ef17ce3b543722878ba6cb71272e53dd2e910
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-09-07 01:30:26 +0300

    code cleaned

--- ClassTree/Program.fs
+++ ClassTree/Program.fs
@@ -1,9 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 type fieldType = ArrayOf | SystemType | CustomType 
-
 let printStringWithTabs t s = [0..t]|> List.map (fun x -> "\t") |> (@) [s] |> List.rev|> (@) ["\n"] |> List.iter (fun x-> printf "%s" x)
-
 let getFieldInfoWithNotes (fi:System.Reflection.FieldInfo) = if fi.FieldType.IsArray then ( fieldType.ArrayOf,fi, fi.FieldType.GetElementType()) elif fi.FieldType.Namespace = "System" then ( fieldType.SystemType,fi, fi.FieldType.GetElementType()) else ( fieldType.CustomType,fi, fi.FieldType.GetElementType())
 
 let rec printClassMember (deep: int) (cm:fieldType, fi: System.Reflection.FieldInfo, st:System.Type) = 
@@ -13,7 +11,6 @@ let rec printClassMember (deep: int) (cm:fieldType, fi: System.Reflection.FieldI
                                       printClass (deep+1) (fi.MemberType.GetType())
     | (fieldType.ArrayOf,fi,st) -> printStringWithTabs deep <| sprintf "%s[]" (st.ToString())
                                    printClass (deep+1) (st)
-
 and printClass (deep:int) (c: System.Type ) = 
     printStringWithTabs deep c.Name  
     let temp = c.GetFields( System.Reflection.BindingFlags.NonPublic ||| System.Reflection.BindingFlags.Instance ) |> Array.filter (fun x -> (x.MemberType.ToString()) = "Field" && (x.Name.EndsWith("Field")) ) |> Array.toList |> List.map getFieldInfoWithNotes |> List.sortBy (fun (x,_,_)-> x) |> List.iter (fun x-> (printClassMember deep) x)
