ba9848fd6827bf02e114193a97ef58a6615f8526
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-09-06 18:53:02 +0300

    refactoring

--- ClassTree/Program.fs
+++ ClassTree/Program.fs
@@ -2,31 +2,36 @@
 // See the 'F# Tutorial' project for more help.
 type fieldType = ArrayOf | SystemType | CustomType 
 
-
 let getAssembly assemblyPath = System.Reflection.Assembly.LoadFile(assemblyPath)
 let getAssemblyClasses (assembly: System.Reflection.Assembly) = assembly.GetTypes()
+
 let printStringWithTabs t s = [0..t]|> List.map (fun x -> "\t") |> (@) [s] |> List.rev|> (@) ["\n"] |> List.iter (fun x-> printf "%s" x)
 let printfMemberOfSystemType t (m:System.Reflection.FieldInfo) = printStringWithTabs t (sprintf "%s : %s" m.Name (m.FieldType.ToString()))
+
 let getFieldInfoWithNotes (fi:System.Reflection.FieldInfo)= 
     if fi.FieldType.IsArray then
-        (fi, fieldType.ArrayOf, fi.FieldType.GetElementType())
-    elif fi.FieldType.Namespace = "System"
-        (fi, fieldType.SystemType, fi.FieldType.GetElementType())
-    else
-        (fi, fieldType.CustomType, fi.FieldType.GetElementType())
- 
-let rec printfCustomType (deep:int) (m:System.Reflection.MemberInfo) = 
-    printStringWithTabs deep m.Name 
-    printClass (deep+1) (m.MemberType.GetType())
+        ( fieldType.ArrayOf,fi, fi.FieldType.GetElementType())
+    elif fi.FieldType.Namespace = "System" then
+        ( fieldType.SystemType,fi, fi.FieldType.GetElementType())
+    else 
+        ( fieldType.CustomType,fi, fi.FieldType.GetElementType())
 
-and printClass (deep:int) (c: System.Type )= 
+let printClassMember (deep: int) cm = 
+    match cm with
+    | (fieldType.SystemType,fi,st) -> printfMemberOfSystemType ( deep + 1) fi
+    | (fieldType.CustomType,fi,st) -> printStringWithTabs deep fi.Name 
+                                      printClass (deep+1) (fi.MemberType.GetType())
+    | (fieldType.ArrayOf,fi,st) -> printStringWithTabs deep <| sprintf "%s[]" (st.ToString()
+                                   printClass (deep+1) (st)
+
+let printClass (deep:int) (c: System.Type )= 
     printStringWithTabs deep c.Name  
     let temp = c.GetFields( System.Reflection.BindingFlags.NonPublic ||| System.Reflection.BindingFlags.Instance )|> Array.filter (fun x -> (x.MemberType.ToString()) = "Field" && (x.Name.EndsWith("Field")) ) |> Array.toList
-    let systemTypes, customTypes =  temp|> List.partition ( fun cm -> cm.FieldType.Namespace = "System")
-    let customGenericTypes, customSimpleTypes =  customTypes |> List.partition ( fun cm -> ( cm.FieldType.BaseType.ToString() <> "System.Object" ) )
-    systemTypes |> List.sortBy (fun x -> x.Name) |> List.iter (printfMemberOfSystemType (deep+1))
-    customSimpleTypes |> List.sortBy (fun x -> x.Name) |> List.iter (printfCustomType (deep+1))
-    customGenericTypes |> List.sortBy (fun x -> x.Name) |> List.iter (printfCustomType (deep+1))
+    let customTypes =  temp |> List.map getFieldInfoWithNotes |> List.sortBy (fun (x,_,_)-> x) |> List.iter (fun x-> (printClassMember deep) x)
+    //let customGenericTypes, customSimpleTypes =  customTypes |> List.partition ( fun cm -> ( cm.FieldType.BaseType.ToString() <> "System.Object" ) )
+    //systemTypes |> List.sortBy (fun x -> x.Name) |> List.iter (printfMemberOfSystemType (deep+1))
+    //customSimpleTypes |> List.sortBy (fun x -> x.Name) |> List.iter (printfCustomType (deep+1))
+    //customGenericTypes |> List.sortBy (fun x -> x.Name) |> List.iter (printfCustomType (deep+1))
 
 [<EntryPoint>]
 let main argv = 
